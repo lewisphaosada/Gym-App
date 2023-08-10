264850f6756caa1c5a3fa5d072d50b6a3b6b384d
sadam hussein <sadamhhh@icloud.com>
2023-08-10 16:53:31 -0500

    employePortalUpdate

--- vue/src/views/EmployeePortal.vue
+++ vue/src/views/EmployeePortal.vue
@@ -1,18 +1,27 @@
 <template>
-    <employee-portal></employee-portal>
+  <div class="employee-portal">
+    <h2>Welcome to the Employee Portal</h2>
+    <p>This is where employees can access special features and functionalities.</p>
+    <!-- Add more content or components here -->
+  </div>
 </template>
 
 <script>
 import EmployeePortal from '@/components/EmployeePortal.vue'
 
 export default {
-    components: {
-        EmployeePortal
-    }
-}
+    name: "EmployeePortal"
+    
+};
 </script>
 
 <style scoped>
-
-
+.employee-portal {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f8f8f8;
+  text-align: center;
+}
 </style>
