72cd2848dc6f14e63e54541951cb807c2d147d23
Ryan Anderson <ryan.anderson.3939@gmail.com>
2023-08-10 12:45:34 -0500

    Sync with sessions

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
   state: {
     token: currentToken || '',
     user: currentUser || {},
+    isCheckedIn : false
 },
   mutations: {
     SET_AUTH_TOKEN(state, token) {
