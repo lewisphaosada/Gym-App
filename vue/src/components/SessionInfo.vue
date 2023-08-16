bcf11bd9e6d6c3e72679e29325f48f965ff440a2
Gavin Campbell <gavin.dy.campbell@gmail.com>
2023-08-16 15:47:32 -0500

    session workouts displayinh on session info

--- vue/src/components/SessionInfo.vue
+++ vue/src/components/SessionInfo.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-      Test
+      {{sessionWorkouts}}
   </div>
 </template>
 
 <script>
+import WorkoutService from '../services/WorkoutService'
+
 export default {
-  name: 'session-comp'
+  name: 'session-comp',
+  data() {
+    return {
+      sessionWorkouts: []
+    }
+  },
+  created() {
+    this.populateSessionWorkouts();
+    // console.log(this.sessionWorkouts)
+  },
+  methods: {
+    populateSessionWorkouts() {
+      WorkoutService.listBySessionId(this.$route.params.id).then(response => {
+        this.sessionWorkouts = response.data;
+      })
+    }
+  }
 }
 </script>
 
